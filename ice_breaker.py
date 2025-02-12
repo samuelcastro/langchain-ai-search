6e643104d94d8577b4de78ac412b603fd51e31a0
Samuel Silva <samuelcastrosilva@gmail.com>
2025-02-12 17:08:22 -0500

    Scraping LinkedIn data

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,4 +1,4 @@
-# Output parsers 
+# Output parsers
 from langchain_core.output_parsers import StrOutputParser
 
 # Prompt Templates: Prompt Templates in LangChain are a way to create reusable prompts for different chains.
@@ -7,6 +7,8 @@ from langchain_core.prompts import PromptTemplate
 # Chain Models: Chain Models in LangChain are a way to chain together multiple components to form a larger chain.
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+
+from third_parties.linkedin import scrape_linkedin_profile
 
 from dotenv import load_dotenv
 import os
@@ -20,12 +22,6 @@ if not api_key:
         "OPENAI_API_KEY not found in environment variables. Please check your .env file."
     )
 
-information = """
-Elon Reeve Musk (/ˈiːlɒn mʌsk/; born June 28, 1971) is a businessman and U.S. special government employee, best known for his key roles in Tesla, Inc. and SpaceX, and his ownership of Twitter. Musk is the wealthiest individual in the world; as of February 2025, Forbes estimates his net worth to be US$397 billion. He has received several honors for his design and engineering work.
-
-A member of the wealthy South African Musk family, Musk was born in Pretoria before immigrating to Canada, acquiring its citizenship. He moved to California in 1995 to attend Stanford University, and with his brother Kimbal co-founded the software company Zip2, that was later acquired by Compaq in 1999. That same year, Musk co-founded X.com, a direct bank, that later formed PayPal. In 2002, Musk acquired U.S. citizenship, and eBay acquired PayPal. Using the money he made from the sale, Musk founded SpaceX, a spaceflight services company, in 2002. In 2004, Musk was an early investor in electric vehicle manufacturer Tesla and became its chairman and later CEO. In 2018, the U.S. Securities and Exchange Commission (SEC) sued Musk for fraud, alleging he falsely announced that he had secured funding for a private takeover of Tesla; he stepped down as chairman and paid a fine. In 2022, he acquired Twitter, and rebranded the service as X the following year.
-"""
-
 # This script prints a simple message when run directly.
 # The special variable __name__ is set to "__main__" when the script is executed directly,
 # as opposed to being imported into another module.
@@ -33,20 +29,26 @@ if __name__ == "__main__":
     print("Hello, LangChain test!")
 
     summary_template = """
-    what model are you?
+    Given the information {information} about a person from I want you to create:
+    1. a short summary
+    2. two interesting facts about them
 """
 
     summary_prompt = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=api_key)
-    llm = ChatOllama(model="mistral", temperature=0)
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=api_key)
+    # llm = ChatOllama(model="mistral", temperature=0)
 
     # Chain: A chain is a sequence of components that are executed in order.
     # Output Parsers: Output Parsers in LangChain are a way to parse the output of a chain.
-    chain = summary_prompt | llm | StrOutputParser()
+    chain = summary_prompt | llm
 
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/samuelcasilva/"
+    )
+
+    res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkedIn profile
+    """
+
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/samuelcastro/afafea0032854ab53d647edc05683435/raw/3bcb3cf3cfeb52baa0f7e3e2e3ecae47a2790b55/sam-silva-data.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
+
+    else:
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.getenv("SCRAPIN_API_KEY"),
+            "linkedInUrl": linkedin_profile_url,
+        }
+
+        response = requests.get(api_endpoint, params=params, timeout=10)
+
+        data = response.json().get("person")
+        # Create a new dictionary by filtering out empty/null values and specific keys
+        # - Removes any values that are empty lists, empty strings, or None
+        # - Excludes the keys "peopleAlsoViewed" and "certifications"
+        # This cleans up the LinkedIn profile data to only include meaningful information
+        data = {
+            k: v
+            for k, v in data.items()
+            if v not in ([], "", "", None)
+            and k not in ["peopleAlsoViewed", "certifications"]
+        }
+        return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/samuelcasilva/", mock=True
+        )
+    )
