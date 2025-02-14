bc974dae593e4fff2f0e9e026def394ec363e68b
Samuel Silva <samuelcastrosilva@gmail.com>
2025-02-14 12:13:19 -0500

    fix: Added output parser using pydantic

--- ice_breaker.py
+++ ice_breaker.py
@@ -15,6 +15,7 @@ from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 
 from dotenv import load_dotenv
 from third_parties.twitter import scrape_user_tweets
+from output_parsers import summary_parser
 
 load_dotenv(override=True)
 
@@ -28,7 +29,7 @@ load_dotenv(override=True)
 
 def ice_breaker_with(name: str) -> str:
     linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
 
     twitter_url = twitter_lookup_agent(name=name)
     tweets = scrape_user_tweets(username=twitter_url, mock=True)
@@ -40,17 +41,22 @@ def ice_breaker_with(name: str) -> str:
         2. two interesting facts about them
         
         Use both the information from LinkedIn and the twitter posts.
+
+        \n{format_instructions}
     """
 
     summary_prompt = PromptTemplate(
-        input_variables=["information", "twitter_posts"], template=summary_template
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
     )
 
     llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
     # Chain: A chain is a sequence of components that are executed in order.
     # Output Parsers: Output Parsers in LangChain are a way to parse the output of a chain.
-    chain = summary_prompt | llm | StrOutputParser()
+    # chain = summary_prompt | llm | StrOutputParser()
+    chain = summary_prompt | llm | summary_parser
 
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url="https://www.linkedin.com/in/samuelcasilva/", mock=True
